cd57b03db9cf573272cc64f67b5030a8fc456589
David Swaintek <dswaintek@gmail.com>
2015-10-13 13:57:45 -0700

    Added button functionality and score tracking to images

--- script.js
+++ script.js
@@ -5,8 +5,24 @@ var photoFiles = ['abba.jpg', 'kevinrowland.jpg', 'bajo.jpg', 'liebe.jpg',
 					'godschild.jpg', 'tino.jpg', 'gunther.jpg', 'waynecochranegoinbacktomiami.jpg', 
 					'kenbyrequestonly.jpg'];
 
+var albumArray = [];
+
+var img1;
+
+var img2;
+
+var Album = function (album, score) {
+	this.album = album;
+	this.score = score;
+};
+
+for (var i = 0; i < photoFiles.length; i += 1) {
+	var newAlbum = new Album(photoFiles[i], 0);
+	albumArray.push(newAlbum);
+};
+
 var randImg = function () {
-		return Math.floor(Math.random() * photoFiles.length);
+		return Math.floor(Math.random() * albumArray.length);
 	};
 
 var addImages = function () {
@@ -14,20 +30,34 @@ var addImages = function () {
 	var img2Loc = document.getElementById('img2');
 	if (img1Loc.firstChild) {
 		img1Loc.removeChild(img1Loc.firstChild);
-	}
+	};
 	if (img2Loc.firstChild) {
 		img2Loc.removeChild(img2Loc.firstChild);
-	}
+	};
 	var img1El = document.createElement('img');
-	img1El.src = 'img/' + photoFiles[randImg()];
+	img1 = randImg();
+	img1El.src = 'img/' + albumArray[img1].album;
 	img1Loc.appendChild(img1El);
 	var img2El = document.createElement('img');
 	do {
-	img2El.src = 'img/' + photoFiles[randImg()];
+	img2 = randImg();
+	img2El.src = 'img/' + albumArray[img2].album;
 	} while (img2El.src == img1El.src);
 	img2Loc.appendChild(img2El);
-}
+};
 
 addImages();
-var picButton = document.getElementById('picButton');
-picButton.addEventListener('click', addImages)
+
+var img1Button = document.getElementById('img1');
+img1Button.addEventListener('click', function() {
+	albumArray[img1].score += 1;
+	console.log(albumArray[img1]);
+	addImages();
+});
+
+var img2Button = document.getElementById('img2');
+img2Button.addEventListener('click', function() {
+	albumArray[img2].score += 1;
+	console.log(albumArray[img2]);
+	addImages();
+});
